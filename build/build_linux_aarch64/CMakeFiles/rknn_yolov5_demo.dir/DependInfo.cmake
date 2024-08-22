1712e5fbb3a000d2495d88758bf5f575cd5a4efa
hydrogenbond001 <119727587+hydrogenbond001@users.noreply.github.com>
2024-08-22 13:41:33 +0800

    Add files via upload

---
+++ build/build_linux_aarch64/CMakeFiles/rknn_yolov5_demo.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/orangepi/rknn-cpp-Multithreading-main/src/camera.cc" "CMakeFiles/rknn_yolov5_demo.dir/src/camera.cc.o" "gcc" "CMakeFiles/rknn_yolov5_demo.dir/src/camera.cc.o.d"
+  "/home/orangepi/rknn-cpp-Multithreading-main/src/postprocess.cc" "CMakeFiles/rknn_yolov5_demo.dir/src/postprocess.cc.o" "gcc" "CMakeFiles/rknn_yolov5_demo.dir/src/postprocess.cc.o.d"
+  "/home/orangepi/rknn-cpp-Multithreading-main/src/preprocess.cc" "CMakeFiles/rknn_yolov5_demo.dir/src/preprocess.cc.o" "gcc" "CMakeFiles/rknn_yolov5_demo.dir/src/preprocess.cc.o.d"
+  "/home/orangepi/rknn-cpp-Multithreading-main/src/rkYolov5s.cc" "CMakeFiles/rknn_yolov5_demo.dir/src/rkYolov5s.cc.o" "gcc" "CMakeFiles/rknn_yolov5_demo.dir/src/rkYolov5s.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
