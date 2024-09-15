9939baad463b6157d29c3696da20de2d6d1a6e97
hydrogenbond001 <119727587+hydrogenbond001@users.noreply.github.com>
2024-09-16 04:03:41 +0800

    Update camera.cc

--- src/camera.cc
+++ src/camera.cc
@@ -11,8 +11,6 @@
 #include "rknn_api.h"
 #include "preprocess.h"
 
-#define PERF_WITH_POST 1
-
 // Function prototypes
 static void dump_tensor_attr(rknn_tensor_attr *attr);
 double __get_us(struct timeval t);
@@ -103,26 +101,23 @@ static int saveFloat(const char *file_name, float *output, int element_size)
 }
 
 
+
+
+
+
 int main(int argc, char **argv)
 {
-    if (argc < 2)
-    {
-        printf("Usage: %s <rknn model>\n", argv[0]);
-        return -1;
-    }
-
     int ret;
     rknn_context ctx;
-    size_t actual_size = 0;
     int img_width = 0;
     int img_height = 0;
     int img_channel = 0;
-    const float nms_threshold = NMS_THRESH;      // Default NMS threshold
-    const float box_conf_threshold = BOX_THRESH; // Default confidence threshold
+    const float nms_threshold = NMS_THRESH;
+    const float box_conf_threshold = BOX_THRESH;
     struct timeval start_time, stop_time;
-    char *model_name = (char *)argv[1];
 
-    // Initialize RKNN context
+    char *model_name = "/home/orangepi/rknn-cpp-Multithreading-main/model/RK3588/yolov5s-640-640.rknn";
+    
     printf("Loading model...\n");
     int model_data_size = 0;
     unsigned char *model_data = load_model(model_name, &model_data_size);
@@ -133,15 +128,6 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    rknn_sdk_version version;
-    ret = rknn_query(ctx, RKNN_QUERY_SDK_VERSION, &version, sizeof(rknn_sdk_version));
-    if (ret < 0)
-    {
-        printf("rknn_query error ret=%d\n", ret);
-        return -1;
-    }
-    printf("SDK version: %s, Driver version: %s\n", version.api_version, version.drv_version);
-
     rknn_input_output_num io_num;
     ret = rknn_query(ctx, RKNN_QUERY_IN_OUT_NUM, &io_num, sizeof(io_num));
     if (ret < 0)
@@ -149,7 +135,6 @@ int main(int argc, char **argv)
         printf("rknn_query error ret=%d\n", ret);
         return -1;
     }
-    printf("Model input num: %d, output num: %d\n", io_num.n_input, io_num.n_output);
 
     rknn_tensor_attr input_attrs[io_num.n_input];
     memset(input_attrs, 0, sizeof(input_attrs));
@@ -179,20 +164,16 @@ int main(int argc, char **argv)
     int height = 0;
     if (input_attrs[0].fmt == RKNN_TENSOR_NCHW)
     {
-        printf("Model is NCHW input format\n");
         channel = input_attrs[0].dims[1];
         height = input_attrs[0].dims[2];
         width = input_attrs[0].dims[3];
     }
     else
     {
-        printf("Model is NHWC input format\n");
         height = input_attrs[0].dims[1];
         width = input_attrs[0].dims[2];
         channel = input_attrs[0].dims[3];
     }
-
-    printf("Model input height=%d, width=%d, channel=%d\n", height, width, channel);
 
     rknn_input inputs[1];
     memset(inputs, 0, sizeof(inputs));
@@ -202,23 +183,8 @@ int main(int argc, char **argv)
     inputs[0].fmt = RKNN_TENSOR_NHWC;
     inputs[0].pass_through = 0;
 
-
-    // 获取并打印OpenCV的构建信息
-    //std::string build_info = cv::getBuildInformation();
-    //std::cout << build_info << std::endl;
-    // Initialize camera
-    //cv::VideoCapture cap(0); // 0是默认摄像头的ID
-    //cv::VideoCapture cap;
-    const char* video_path = argv[2]; // 从命令行获取视频文件路径
-    if (!std::ifstream(video_path))
-    {
-        printf("Video file does not exist: %s\n", video_path);
-        return -1;
-    }
-    //printf("Attempting to open video file: %s\n", video_path);
-    cv::VideoCapture cap(video_path); // 读取视频文件
-    //cap.read(video_path);
-
+    const char* video_path = "/home/orangepi/rknn-cpp-Multithreading-main/jntm.mp4";
+    cv::VideoCapture cap(video_path);
     if (!cap.isOpened())
     {
         printf("Error opening video capture\n");
@@ -235,42 +201,31 @@ int main(int argc, char **argv)
             break;
         }
 
-        //cv::Mat img;
+        // Convert to RGB
         cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
 
+        // Resize image if needed
+        cv::Mat resized_img;
         cv::Size target_size(width, height);
-        cv::Mat resized_img(target_size.height, target_size.width, CV_8UC3);
-        BOX_RECT pads;
-        memset(&pads, 0, sizeof(BOX_RECT));
         float scale_w = (float)target_size.width / img.cols;
         float scale_h = (float)target_size.height / img.rows;
+        float min_scale = std::min(scale_w, scale_h);
+        BOX_RECT pads;
+        memset(&pads, 0, sizeof(BOX_RECT));
+        letterbox(img, resized_img, pads, min_scale, target_size);
 
-        if (img.cols != width || img.rows != height)
-        {
-            // Resize image
-            float min_scale = std::min(scale_w, scale_h);
-            scale_w = min_scale;
-            scale_h = min_scale;
-            letterbox(img, resized_img, pads, min_scale, target_size);
-        }
-        else
-        {
-            resized_img = img;
-        }
-
+        // Set input data
         inputs[0].buf = resized_img.data;
-
         gettimeofday(&start_time, NULL);
         rknn_inputs_set(ctx, io_num.n_input, inputs);
 
+        // Run inference
         rknn_output outputs[io_num.n_output];
         memset(outputs, 0, sizeof(outputs));
         for (int i = 0; i < io_num.n_output; i++)
         {
             outputs[i].want_float = 0;
         }
-
-        // Run inference
         ret = rknn_run(ctx, NULL);
         ret = rknn_outputs_get(ctx, io_num.n_output, outputs, NULL);
         gettimeofday(&stop_time, NULL);
@@ -286,7 +241,7 @@ int main(int argc, char **argv)
             out_zps.push_back(output_attrs[i].zp);
         }
         post_process((int8_t *)outputs[0].buf, (int8_t *)outputs[1].buf, (int8_t *)outputs[2].buf, height, width,
-                     box_conf_threshold, nms_threshold, pads, scale_w, scale_h, out_zps, out_scales, &detect_result_group);
+                     box_conf_threshold, nms_threshold, pads, min_scale, min_scale, out_zps, out_scales, &detect_result_group);
 
         // Draw results
         for (int i = 0; i < detect_result_group.count; i++)
@@ -294,8 +249,6 @@ int main(int argc, char **argv)
             detect_result_t *det_result = &(detect_result_group.results[i]);
             char text[256];
             sprintf(text, "%s %.1f%%", det_result->name, det_result->prop * 100);
-            printf("%s @ (%d %d %d %d) %f\n", det_result->name, det_result->box.left, det_result->box.top,
-                   det_result->box.right, det_result->box.bottom, det_result->prop);
             int x1 = det_result->box.left;
             int y1 = det_result->box.top;
             int x2 = det_result->box.right;
@@ -306,18 +259,23 @@ int main(int argc, char **argv)
 
         // Show image
         cv::imshow("Detection", img);
-        
-        if (cv::waitKey(1) >= 0) break; // Break on any key press
+
+        // Release RKNN outputs
+        rknn_outputs_release(ctx, io_num.n_output, outputs);
+
+        // Wait for user input to exit
+        if (cv::waitKey(1) >= 0) break;
+
+        // Free the resized image memory
+        resized_img.release();
     }
 
     // Cleanup
-    rknn_destroy(ctx);
     cap.release();
+    rknn_destroy(ctx);
     cv::destroyAllWindows();
-    if (model_data)
-    {
-        free(model_data);
-    }
+    free(model_data);
+    deinitPostProcess(); // Release post-process resources
 
     return 0;
 }
